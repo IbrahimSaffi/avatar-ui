4a0c1800ebcbef80ba057e1d049c0905afb8cb55
IbrahimSaffi <ibrahimsaffinew@gmail.com>
2022-09-06 11:41:27 +0500

    complete

--- src/components/InnerContainer.jsx
+++ src/components/InnerContainer.jsx
@@ -8,9 +8,16 @@ export default function InnerContainer(props) {
         <p className="label">Choose your avatar</p>
         <div className="avatars">
             {avatars.map((avatar,i)=>{
-             return <div className='animate' >
-                <div className="a-border"></div>
-                 <img onClick={()=>props.handleClick(i)} className={i===props.movingBorder?"avatar animate":(i===props.selected?"avatar selected":"avatar")}  src={avatar.src} alt="" srcset="" />
+             return <div style={{backgroundColor:i===props.movingBorder? "#69737a":"#2C2B2B"}} className='animate' >
+                <div className="animated-border"></div>
+                 <img onClick={()=>
+                  {
+                    if(props.movingBorder===null){
+                      props.handleClick(i)
+                    }
+                   
+                  }
+                  } className={i===props.movingBorder?"avatar animate":(i===props.selected?"avatar selected":"avatar")}  src={avatar.src} alt="" srcset="" />
                 </div>
             })}
         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,13 +11,15 @@ export default function Main(){
             setSelected(i)
             setMovingBorder(null)
             setCardDisplay(false)
+
         },2000)
+
         setMovingBorder(i)
     }
     return (
         <div className="outer-container">
             <img style={{border:cardDisplay?"1px solid #69737a":""}}  onClick={()=>!cardDisplay?setCardDisplay(true):setCardDisplay(false)} className="curr img"  src={avatars[selected].src}  alt="" />
-           <InnerContainer handleClick ={handleClick} cardDisplay={cardDisplay} movingBorder ={movingBorder} selected = {selected} />
+           <InnerContainer  handleClick ={handleClick} cardDisplay={cardDisplay} movingBorder ={movingBorder} selected = {selected} />
         </div>
     )
 }
